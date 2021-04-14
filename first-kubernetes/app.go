95390ff00a7d2504133f3391436b014b499f5e3a
arthurh0812 <arthurh1204@gmail.com>
2021-04-14 13:09:47 +0200

    cleanup

--- first-kubernetes/app.go
+++ first-kubernetes/app.go
@@ -1,1 +1,56 @@
-package first_kubernetes
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/arthurh0812/first-kubernetes/tasks"
+)
+
+var port string
+
+func init() {
+	port = os.Getenv("PORT")
+	if port == "" { // default port
+		port = "8080"
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		_, err := w.Write([]byte("Hi there! My name is Max"))
+		if err != nil {
+			log.Fatalf("error writing response: %v", err)
+		}
+
+		if flusher, ok := w.(http.Flusher); ok {
+			flusher.Flush()
+		}
+	})
+
+	mux.HandleFunc("/tasks", func(w http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case http.MethodGet:
+			tasks.GetTasks(w, req)
+		case http.MethodPost:
+			c := tasks.NewTaskCreator(w, req)
+			c.CreateTask()
+		case http.MethodPatch:
+			tasks.UpdateTask(w, req)
+		}
+	})
+
+	srv := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
--- first-kubernetes/go.mod
+++ first-kubernetes/go.mod
@@ -1,1 +1,9 @@
-module "first-kubernetes"
+module github.com/arthurh0812/first-kubernetes
+
+go 1.15
+
+require (
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+)
