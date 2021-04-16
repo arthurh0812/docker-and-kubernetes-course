bef31f8ed43cb73b6447865bd46a00660d38dcc1
arthurh0812 <arthurh1204@gmail.com>
2021-04-16 15:55:18 +0200

    Extended functionality that lets the application consume the little story data API service running on the same GKE cluster

--- kub-consumer/main.go
+++ kub-consumer/main.go
@@ -5,22 +5,44 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
+var service string
+var resource string
+
+func init() {
+	service = os.Getenv("SERVICE")
+	if service == "" {
+		service = "localhost:80"
+	}
+	resource = os.Getenv("RESOURCE")
+	if resource == "" {
+		resource = "story"
+	}
+}
+
 func main() {
-	resp, err := http.Get("10.132.1.208:80/story")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		resp, err := http.Get(fmt.Sprintf("http://%s/%s", service, resource))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		bs, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		n := len(bs)
+		fmt.Fprintf(w, "communication worked: %d bytes transmitted\n", n)
+		fmt.Fprintln(w, string(bs))
+	})
+
+	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	bs, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	n := len(bs)
-	fmt.Printf("communication worked: %d bytes transmitted\n", n)
-	fmt.Println(string(bs))
-
-	http.ListenAndServe(":80", nil)
 }
